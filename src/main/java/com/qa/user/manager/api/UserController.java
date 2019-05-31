3873729afd9cde6231a21d1fefd67d317ff3b9bb
Daniel Smith <danielsmith.2796@googlemail.com>
2019-05-31 12:12:40 +0100

    added example data to UserController

--- src/main/java/com/qa/user/manager/api/UserController.java
+++ src/main/java/com/qa/user/manager/api/UserController.java
@@ -6,9 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
+	private User[] allUsers = 
+		{
+			new User(1, "John", 33),
+			new User(2, "James", 45),
+			new User(3, "Jane", 24),
+			new User(4, "Dan", 22),
+			new User(5, "Kane", 43),
+			new User(6, "Frank", 64),
+			new User(7, "Trent", 31),
+			new User(8, "Link", 23),
+			new User(9, "Dean", 46)
+		};
+	
 	@GetMapping(value="/users")
-	public String getAllUsers() {
-		return "hello";
+	public User[] getAllUsers() {
+		return ;
 	}
 	
 }
